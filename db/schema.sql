cdbc581fbdc9bccced72e0123b258bcc792ed569
conner <conner.williams4@gmail.com>
2023-05-27 16:56:29 -0400

    final touch ups

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,37 @@ DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
+
+-- Use the created database
+USE ecommerce_db;
+
+-- Create the Category table
+CREATE TABLE categories (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(50) NOT NULL
+);
+
+-- Create the Product table
+CREATE TABLE products (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_name VARCHAR(100) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  stock INT NOT NULL,
+  category_id INT,
+  FOREIGN KEY (category_id) REFERENCES categories (id)
+);
+
+-- Create the Tag table
+CREATE TABLE tags (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(50) NOT NULL
+);
+
+-- Create the ProductTag table
+CREATE TABLE product_tags (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_id INT,
+  tag_id INT,
+  FOREIGN KEY (product_id) REFERENCES products (id),
+  FOREIGN KEY (tag_id) REFERENCES tags (id)
+);
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,32 +1,3 @@
-// const router = require('express').Router();
-// const { Category, Product } = require('../../models');
-
-// // The `/api/categories` endpoint
-
-// router.get('/', (req, res) => {
-//   // find all categories
-//   // be sure to include its associated Products
-// });
-
-// router.get('/:id', (req, res) => {
-//   // find one category by its `id` value
-//   // be sure to include its associated Products
-// });
-
-// router.post('/', (req, res) => {
-//   // create a new category
-// });
-
-// router.put('/:id', (req, res) => {
-//   // update a category by its `id` value
-// });
-
-// router.delete('/:id', (req, res) => {
-//   // delete a category by its `id` value
-// });
-
-// module.exports = router;
-
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
@@ -34,8 +5,13 @@ const { Category, Product } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
-    // Find all categories and include associated Products
-    const categories = await Category.findAll({ include: Product });
+    const categories = await Category.findAll({
+      include: {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock'],
+      },
+    });
+
     res.json(categories);
   } catch (err) {
     console.log(err);
@@ -45,12 +21,18 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try {
-    // Find one category by its `id` value and include associated Products
-    const category = await Category.findByPk(req.params.id, { include: Product });
+    const category = await Category.findByPk(req.params.id, {
+      include: {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock'],
+      },
+    });
+
     if (!category) {
       res.status(404).json({ message: 'Category not found' });
       return;
     }
+
     res.json(category);
   } catch (err) {
     console.log(err);
@@ -60,7 +42,6 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    // Create a new category
     const category = await Category.create(req.body);
     res.status(201).json(category);
   } catch (err) {
@@ -71,14 +52,15 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   try {
-    // Update a category by its `id` value
     const updatedCategory = await Category.update(req.body, {
       where: { id: req.params.id },
     });
+
     if (!updatedCategory[0]) {
       res.status(404).json({ message: 'Category not found' });
       return;
     }
+
     res.status(200).json({ message: 'Category updated successfully' });
   } catch (err) {
     console.log(err);
@@ -88,14 +70,15 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    // Delete a category by its `id` value
     const deletedCategory = await Category.destroy({
       where: { id: req.params.id },
     });
+
     if (!deletedCategory) {
       res.status(404).json({ message: 'Category not found' });
       return;
     }
+
     res.status(200).json({ message: 'Category deleted successfully' });
   } catch (err) {
     console.log(err);
